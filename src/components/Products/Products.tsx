58c3c3eb7cf72644e3e225c8a802971e98a30273
manuelaperezsimon <manuela@swipoo.com>
2024-09-05 09:53:29 +0200

    change style in Products and change test to consider context

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import ProductCard from "../Product/Product";
 import { Product } from "@/interfaces/product";
+import Cart from "../Cart/Cart";
 
 interface ProductsProps {
   products: Product[];
@@ -9,14 +10,24 @@ interface ProductsProps {
 
 const Products = ({ products }: ProductsProps): JSX.Element => {
   return (
-    <div className="p-4">
-      <div
-        data-testid="container"
-        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4"
-      >
-        {products.map((product) => (
-          <ProductCard key={product.code} product={product} />
-        ))}
+    <div className="flex flex-col md:flex-row">
+      <div className="flex-1 p-4">
+        <div
+          data-testid="container"
+          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4"
+        >
+          {products.map((product) => (
+            <ProductCard key={product.code} product={product} />
+          ))}
+        </div>
+      </div>
+
+      <div className="hidden md:block md:w-96">
+        <Cart />
+      </div>
+
+      <div className="md:hidden fixed bottom-0 left-0 w-full bg-white shadow-lg rounded-t-lg p-4 z-50">
+        <Cart />
       </div>
     </div>
   );
