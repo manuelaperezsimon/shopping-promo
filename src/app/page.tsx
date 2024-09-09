f03d8670460a0c99cfce90b2c236d350353ab2ee
manuelaperezsimon <manuela@swipoo.com>
2024-09-09 11:49:04 +0200

    add Head Next component in page and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import { CartProvider } from "../context/Shopping.context";
 import products from "../data/products.json";
 import { Product } from "../interfaces/product";
@@ -9,6 +10,13 @@ export default function Home() {
 
   return (
     <CartProvider>
+      <Head>
+        <title>Doc Morris</title>
+        <meta
+          name="description"
+          content="Explora nuestra amplia gama de productos en Tu Tienda Online. Encuentra lo que necesitas con precios competitivos y una experiencia de compra sin igual."
+        />
+      </Head>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <Products products={productsTyped} />
       </main>
